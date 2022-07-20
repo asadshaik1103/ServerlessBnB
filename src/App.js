ab9bd9e4e15062befe1d7e5f9bdf5ff6d55e9ef6
Shaik Asaduddin <shaikasaduddin@TABD5.WPA.Dal.Ca>
2022-07-19 23:20:01 -0300

    fix(ui: )merge commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import { CipherComponent } from './components/caesarCipher/CipherComponent';
 import { SignIn } from './components/authentication/login';
 import { QnAAuthentication } from './components/authentication/qna-authentication';
 import { CipherComponentAuthentication } from './components/authentication/caesarCipher-authentication';
+import Home from './components/Home';
+import { Feedback } from './components/Feedback/feedback';
+import { Analysis } from './components/analysis/analysis';
+
 // import ReactDOM from "react-dom/client";
 import {
   BrowserRouter,
@@ -17,15 +21,18 @@ function App() {
   return (
     // <SignUp />
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<SignUp />} />
-      <Route path="/qna" element={<QnA />} />
-      <Route path="/cipher" element={<CipherComponent />} />
-      <Route path="/login" element={<SignIn />} />
-      <Route path="/qna-authenticate" element={<QnAAuthentication />} />
-      <Route path="/cipher-authenticate" element={<CipherComponentAuthentication />} />
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<SignUp />} />
+        <Route path="/qna" element={<QnA />} />
+        <Route path="/cipher" element={<CipherComponent />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/qna-authenticate" element={<QnAAuthentication />} />
+        <Route path="/cipher-authenticate" element={<CipherComponentAuthentication />} />
+        <Route path='/feedback' element={<Feedback />} />
+        <Route path='/analysis' element={<Analysis />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
