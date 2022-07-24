e496ec949b8d0166ccdc6e66a18217422d417128
Meha Vijaybhai Desai <mh305558@dal.ca>
2022-07-24 12:26:53 -0300

    Added Visualisation and Report Generation

---
+++ src/components/userLogs/userLogs.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function UserLogs() {
+    return (
+        <div>
+            <iframe
+                width="1430"
+                height="820"
+                src="https://datastudio.google.com/embed/reporting/4c29e889-2437-4d18-a42e-4c9f94314cca/page/lbYyC"
+                frameborder="0"
+                style={{ border: 0 }}
+                allowfullscreen>
+            </iframe>
+        </div>
+    )
+}
+
+export default UserLogs
